789543ee2b48b5b0bea895ecc2b28c1a67c0b961
rohitsainier <rohitsainier@gmail.com>
2024-06-19 23:59:49 +0530

    footer ui added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
-import logo from "../../public/assets/logo.png";
+import { logo } from "@/exports/images";
 import Image from "next/image";
 export default function Header() {
   return (
-    <div className="flex mt-9 sm:ml-[200px] mr-[100px] items-center justify-between">
+    <div className="flex mt-9 mb-9 sm:ml-[200px] mr-[100px] items-center justify-between">
       <Image src={logo} alt="logo" height={25}></Image>
       <div className="flex gap-6">
         <p className="hover:underline">SHOP</p>
