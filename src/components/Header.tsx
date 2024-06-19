c01503bb2d6072350ef749b7bb92d6a1ce55653a
rohitsainier <rohitsainier@gmail.com>
2024-06-19 23:22:01 +0530

    header ui added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,13 @@
+import logo from "../../public/assets/logo.png";
+import Image from "next/image";
 export default function Header() {
-  return <div className="text-paragraph">Header</div>;
+  return (
+    <div className="flex mt-9 sm:ml-[200px] mr-[100px] items-center justify-between">
+      <Image src={logo} alt="logo" height={25}></Image>
+      <div className="flex gap-6">
+        <p className="hover:underline">SHOP</p>
+        <p className="hover:underline">ABOUT</p>
+      </div>
+    </div>
+  );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,6 @@ import Header from "./Header";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Button from "./Button";
-import Card from "./Card";
+import { Card, CardType } from "./Card";
 
-export { Header, Navbar, Footer, Button, Card };
+export { Header, Navbar, Footer, Button, Card, CardType };
